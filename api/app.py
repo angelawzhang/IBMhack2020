764ac7f475d9af3980f626790fa4da357e71739d
Valerian Clerc <vclerc@ibm.com>
2020-07-14 17:12:03 -0400

    API CRUD functionality complete

--- api/app.py
+++ api/app.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from tinydb import TinyDB, Query
+
 app = Flask(__name__)
+db = TinyDB('./db.json')
+
+def verify_body(body):
+    try:
+        return body['name'] and body['domain'] and bool(body['blackOwned']) and body['logo'] and body['categories'] and body['photos']
+    except:
+        return False
 
 @app.route('/')
 def root():
@@ -7,16 +16,51 @@ def root():
 
 @app.route('/business', methods=['GET'])
 def index():
-    return 'Index'
+    Business = Query()
+    businesses = db.search(Business['type'] == 'business')
+    return jsonify(businesses), 200
 
 @app.route('/business/<business_name>', methods=['GET'])
 def show(business_name):
-    return 'Show business %s' % business_name
+    Business = Query()
+    businesses = db.search((Business['type'] == 'business') & (Business['name'] == business_name))
+    resp = businesses[0] if len(businesses) > 0 else {}
+    status = 200
+    if resp == {}:
+        status = 404
+    return jsonify(resp), status
 
 @app.route('/business', methods=['POST'])
 def create():
-    return 'Create business'
+    body = request.json
+    valid = verify_body(body)
+    if not valid:
+        return jsonify({'msg': 'invalid request body'}), 400
+    
+    Business = Query()
+    q = db.search((Business['type'] == 'business') & ((Business['name'] == body['name']) | (Business['domain'] == body['domain'])))
+    if len(q) != 0:
+        return jsonify({'msg': 'business with that name or domain already exists'}), 409
+    
+    new_business = {
+        'type': 'business',
+        'name': body['name'],
+        'domain': body['domain'],
+        'blackOwned': bool(body['blackOwned']),
+        'logo': body['logo'],
+        'categories': body['categories'],
+        'photos': body['photos']
+    }
+    db.insert(new_business)
+
+    return jsonify(new_business), 200
 
 @app.route('/business/<business_name>', methods=['DELETE'])
 def delete(business_name):
-    return 'Delete business %s' % business_name
+    Business = Query()
+    q = db.search((Business['type'] == 'business') & (Business['name'] == business_name))
+    if len(q) == 0:
+        return jsonify({'msg': 'business with that name not found'}), 404
+    
+    db.remove((Business['type'] == 'business') & (Business['name'] == business_name))
+    return jsonify({'msg': 'business deleted'}), 200
