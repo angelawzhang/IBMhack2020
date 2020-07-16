2dd88d857e5d91bee03cee09df6b9e0f04dce3c9
Angela Zhang <awzhang@angelas-mbp.attlocal.net>
2020-07-16 14:06:18 -0500

    Added site alternatives

--- extension/scripts/data.js
+++ extension/scripts/data.js
@@ -1,31 +1,38 @@
 const data = [
     {
-        name: "company lmao",
-        domain: "https://google.com",
-        logo: "https://www.creativebrandsmag.com/wp-content/uploads/2020/01/GOOGLE.png",
-        photos: ["https://ih1.redbubble.net/image.91897765.0123/ssrco,unisex_tshirt,womens,fafafa:ca443f4786,front,tall_three_quarter,x1000-bg,f8f8f8.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/images/GGOEGHPB120910.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/largeimages/GGOEGXXX1109.jpg"],
-        tags: "clothing, shoes"
+        name: "Pyer Moss",
+        domain: "https://www.pyermoss.com",
+        logo: "https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/72140191_2467404030145436_2259922326986424320_o.jpg?_nc_cat=102&_nc_sid=85a577&_nc_ohc=BXetGWp0ShsAX9klYIs&_nc_ht=scontent-dfw5-2.xx&oh=c84a10ecf46740f11c32142fa08a4348&oe=5F34CD13",
+        photos: ["https://cdn.shopify.com/s/files/1/2653/3618/products/C3-CLMNT-B-1_Women._1728x.png?v=1591810060", "https://cdn.shopify.com/s/files/1/2653/3618/products/PM_C3_CollegeSlouchSweatpant_Purple_1_Women_1728x.png?v=1588219339", "https://cdn.shopify.com/s/files/1/2653/3618/products/PM_C3_MockNeckLongSleeveTee_black_1_Men_1728x.jpg?v=1589339614"],
+        tags: "Fashion, Clothing, Outerwear"
     },
     {
-        name: "company lol",
-        domain: "https://google.com",
-        logo: "https://www.creativebrandsmag.com/wp-content/uploads/2020/01/GOOGLE.png",
-        photos: ["https://ih1.redbubble.net/image.91897765.0123/ssrco,unisex_tshirt,womens,fafafa:ca443f4786,front,tall_three_quarter,x1000-bg,f8f8f8.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/images/GGOEGHPB120910.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/largeimages/GGOEGXXX1109.jpg"],
-        tags: "tech, plants"
+        name: "Grass-Fields",
+        domain: "https://www.grass-fields.com",
+        logo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/70917144_2438120349772202_4278510372891131904_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=nSLb2ml2cX0AX9-tsiU&_nc_ht=scontent-dfw5-1.xx&oh=5db1b6a316f77249092929d74abac696&oe=5F36069A",
+        photos: ["https://cdn.shopify.com/s/files/1/0736/6629/products/grass-fields-midi-length-dresses-african-print-jameelah-dress-13648615833658_1200x.jpg?v=1581087761", "https://cdn.shopify.com/s/files/1/0736/6629/products/grass-fields-trousers-african-print-nini-shorts-13981185966138_1600x.jpg?v=1591700707", "https://cdn.shopify.com/s/files/1/0736/6629/products/grass-fields-headwrap-african-print-uju-headwrap-14055827669050_1600x.jpg?v=1594037250"],
+        tags: "Fashion, Clothing, Accessories"
     },
     {
-        name: "company wow",
-        domain: "https://google.com",
-        logo: "https://www.creativebrandsmag.com/wp-content/uploads/2020/01/GOOGLE.png",
-        photos: ["https://ih1.redbubble.net/image.91897765.0123/ssrco,unisex_tshirt,womens,fafafa:ca443f4786,front,tall_three_quarter,x1000-bg,f8f8f8.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/images/GGOEGHPB120910.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/largeimages/GGOEGXXX1109.jpg"],
-        tags: "snacks, cars, skincare"
+        name: "Brother Vellies",
+        domain: "https://brothervellies.com/",
+        logo: "https://fashionista.com/.image/t_share/MTYzMDg0NzIyODg3NDY4OTM3/brother-vellies.png",
+        photos: ["https://cdn.shopify.com/s/files/1/1409/0762/products/BV_0015_Grapefruit_Bikeshoe_1000x.jpg?v=1584116989", "https://cdn.shopify.com/s/files/1/1409/0762/products/PhotoCrab_9797_copy_1000x.jpg?v=1588091150", "https://cdn.shopify.com/s/files/1/1409/0762/products/BeirutSlide_1000x.jpg?v=1591421279"],
+        tags: "Shoes, Handbags, Accessories"
     },
     {
-        name: "company no",
-        domain: "https://google.com",
-        logo: "https://www.creativebrandsmag.com/wp-content/uploads/2020/01/GOOGLE.png",
-        photos: ["https://ih1.redbubble.net/image.91897765.0123/ssrco,unisex_tshirt,womens,fafafa:ca443f4786,front,tall_three_quarter,x1000-bg,f8f8f8.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/images/GGOEGHPB120910.jpg", "https://shop.googlemerchandisestore.com/store/20160512512/assets/items/largeimages/GGOEGXXX1109.jpg"],
-        tags: "no, no, no"
+        name: "Johnny Nelson Jewelry",
+        domain: "http://johnnynelson.nyc/",
+        logo: "https://johnnynelson.nyc/wp-content/uploads/2018/06/full-logo_6.png",
+        photos: ["http://johnnynelson.nyc/wp-content/uploads/2019/12/SOJOURNER-TRUTH-PENDANT-1024x1024.png", "http://johnnynelson.nyc/wp-content/uploads/2019/12/ALL-POWER-STUD-NECKLACE-FRONT-1024x1024.png", "http://johnnynelson.nyc/wp-content/uploads/2019/10/RINGS-ECOMM_267-819x1024.jpg"],
+        tags: "Jewelry"
+    },
+    {
+        name: "Armando Cabral",
+        domain: "https://shop.armando-cabral.com/",
+        logo: "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/32116862_10157295186447786_7739653027283337216_n.png?_nc_cat=101&_nc_sid=85a577&_nc_ohc=Fn81XVDI3qsAX-9TAi3&_nc_ht=scontent-dfw5-1.xx&oh=44a46e253dc67ab0a977338012b62205&oe=5F37EB42",
+        photos: ["https://cdn.shopify.com/s/files/1/1020/1461/products/20180405_ARMANDOCABRAL_SS18_MORTON_BLUE_SUEDE_SINGLE_045_1200x.jpg?v=1524446714", "https://cdn.shopify.com/s/files/1/1020/1461/products/20180405_ARMANDOCABRAL_SS18_WARREN_BEIGE_SUEDE_SINGLE_233_1200x.jpg?v=1524446757", "https://cdn.shopify.com/s/files/1/1020/1461/products/Broome_Bianco_SHOE_22_A_10531_3200x_b764d57f-709f-4ea9-adc2-1498b59d94cb_1200x.jpg?v=1515990148"],
+        tags: "Shoes, Sneakers"
     }
     
 ];
